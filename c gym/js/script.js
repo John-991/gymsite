652b261d90009a2d9283b6d792dbc718ebe59914
John-991 <100966951+John-991@users.noreply.github.com>
2022-03-07 23:26:22 +0000

    Add files via upload

---
+++ c gym/js/script.js
@@ -0,0 +1,12 @@
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+var Menu = document.getElementById('navBar')
+
+function showMenu(){
+  Menu.style.right = "0" ;
+}
+
+function hideMenu(){
+  Menu.style.right = "-250px" ;
+}
